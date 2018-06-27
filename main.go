db07bbe28a0a629640284a6f4825f3301d89b636
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2018-06-27 13:03:59 -0500

    declarations

--- main.go
+++ main.go
@@ -2,19 +2,34 @@ package main
 
 import "fmt"
 
+const helloWorld string = "Hola %s %s, bienvenido al fascinante mundo de GO \n"
+const testConst = "Test"
+
 /*
 	go run main.go to execute a file with go
 	go build main.go create a binary file to be execute called ./main(example)
 */
 func main() {
 	var name string
+	name = "Sin nombre"
+	//Declaration and assignment, Go infers the kind of var
+	lastName := "<Modificar>"
+	var number = 100
+	var (
+		a = 1
+		b = 2
+		c = 3
+	)
 	//println ==> line's jump
 	fmt.Print("Ingresa tu nombre: ")
 	//Let us ask a value and use it.
 	fmt.Scanf("%s", &name)
+	fmt.Printf(helloWorld, name, lastName)
 	//Let us print in console
 	//fmt.Print("Hola mundo")
 	//Let us format the text
 	fmt.Printf("Hola %s, bienvenido al fascinante mundo de Go. \n", name)
 	fmt.Println("Hola mundo")
+
+	fmt.Println(number, a, b, c)
 }
