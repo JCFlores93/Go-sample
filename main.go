ef5973390e305f6921ca1dc7c88619a6f54f27e3
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2018-06-27 12:51:15 -0500

    First commit hello world!!

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*
+	go run main.go to execute a file with go
+	go build main.go create a binary file to be execute called ./main(example)
+*/
+func main() {
+	var name string
+	//println ==> line's jump
+	fmt.Println("Ingresa tu nombre: ")
+	//Let us ask a value and use it.
+	fmt.Scanf("%s", &name)
+	//Let us print in console
+	//fmt.Print("Hola mundo")
+	//Let us format the text
+	fmt.Printf("Hola mundo %s, bienvenido", name)
+}
