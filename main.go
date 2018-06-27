e3545cfb41a19b006f2f942fc67c893ea30c5888
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2018-06-27 13:12:35 -0500

    functions in Go

--- main.go
+++ main.go
@@ -10,20 +10,15 @@ const testConst = "Test"
 	go build main.go create a binary file to be execute called ./main(example)
 */
 func main() {
-	var name string
-	name = "Sin nombre"
+	name := getName()
 	//Declaration and assignment, Go infers the kind of var
 	lastName := "<Modificar>"
-	var number = 100
-	var (
-		a = 1
-		b = 2
-		c = 3
-	)
+	number := sum(50, 50)
+	a, b, c := getVariables()
 	//println ==> line's jump
-	fmt.Print("Ingresa tu nombre: ")
+	//fmt.Print("Ingresa tu nombre: ")
 	//Let us ask a value and use it.
-	fmt.Scanf("%s", &name)
+	//fmt.Scanf("%s", &name)
 	fmt.Printf(helloWorld, name, lastName)
 	//Let us print in console
 	//fmt.Print("Hola mundo")
@@ -32,4 +27,25 @@ func main() {
 	fmt.Println("Hola mundo")
 
 	fmt.Println(number, a, b, c)
+}
+
+func getName() string {
+	var name string
+	name = "Sin nombre"
+	fmt.Print("Ingresa tu nombre: ")
+	fmt.Scanf("%s", &name)
+	return name
+}
+
+func getVariables() (int, int, int) {
+	var (
+		a = 1
+		b = 2
+		c = 3
+	)
+	return a, b, c
+}
+
+func sum(a int, b int) int {
+	return a + b
 }
