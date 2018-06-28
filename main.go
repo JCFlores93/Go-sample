69cf61ab5705ac72567fc3baae76fa63ea608834
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2018-06-27 21:56:57 -0500

    switch

--- main.go
+++ main.go
@@ -125,3 +125,22 @@ func strings2() {
 	fmt.Println(strings.Replace(text, "Hello", "Hola", -1))
 	fmt.Println(strings.Split(text, " "))
 }
+
+func switchTest() {
+	var number = 0
+	fmt.Print("[Switch] Ingresa un número del 1 al 10: ")
+	fmt.Scanf("%d", &number)
+	switch number {
+	case 1:
+		fmt.Println("El número es 1 ")
+	default:
+		fmt.Println("El número no es 1")
+	}
+
+	switch {
+	case number%2 == 0:
+		fmt.Println("El número es par")
+	default:
+		fmt.Println("E número no es par")
+	}
+}
